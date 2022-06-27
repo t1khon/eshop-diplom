26151b2d5422f78df849c9f086c9531788e2b326
t1khon <t1khon@protonmail.com>
2022-06-27 23:51:38 +0300

    static

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
